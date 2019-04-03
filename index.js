81c16b840e9afffcfb11b264f3712f630abc2312
Tega Oke <oketegah@gmail.com>
2019-04-03 09:07:26 +0100

    Tetsing another merge conflicts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-//This is a quick file to test git
+//This is a quick file to test gits
 //Adding second line to test merge
 //This is a third line to test merge conflicts
 //A fourth line to test
 //A fourth line for testing
+//Hello world
